99ae3c846cc4e06b4f923c82fb4a467e53af0648
geraldokutar <geraldpilk@gmail.com>
2018-07-24 15:07:57 +0300

    Add another javascipt ruleso that when p in clicked it displays (This is a paragragh)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function(){
   jQuery("h1").click(function(){
     alert("This is a header. ");
+  });
+  jQuery("p").click(function(){
+    alert("This is a paragragh. ");
   })
 })
